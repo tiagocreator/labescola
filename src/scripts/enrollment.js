f59e96f2b85f76604e9f1af70dcc40f5c6edbebb
Tiago Leite <contato@tiagocreator.com>
2023-01-31 14:39:55 -0300

    Add multiple courses option

--- src/scripts/enrollment.js
+++ src/scripts/enrollment.js
@@ -2,22 +2,35 @@ import { alunos } from './data.js';
 
 const newStudentsArr = [...alunos];
 
+let checkboxesArr = [];
+const checkboxes = document.querySelectorAll('.checkbox');
+const studentCourse = document.querySelector('.course');
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', () => {
+    if (!checkboxesArr.includes(checkbox.value)) {
+      checkboxesArr.push(checkbox.value);
+    } else {
+      checkboxesArr.pop(checkbox.value);
+    }
+    studentCourse.value = checkboxesArr.join(', ');
+  });
+});
+
 const form = document.querySelector('.enrollment-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const studentName = document.querySelector('.name').value;
-  const studentCourse = document.querySelector('.course').value;
   const studentClass = document.querySelector('.class').value;
   const studentInstallments = document.querySelector('.installments').value;
 
   newStudentsArr.push({
     estudante: studentName,
-    turma: studentCourse,
-    curso: studentClass,
+    turma: studentClass,
+    curso: (studentCourse.value = checkboxesArr.join(', ')),
     numeroDeParcelas: studentInstallments,
   });
   showResult();
-  console.log(newStudentsArr);
 });
 
 const showResult = () => {
@@ -35,4 +48,9 @@ const showResult = () => {
     }</span></p>
   `;
   form.reset();
+  checkboxesArr = [];
+  studentCourse.value = '';
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
 };
